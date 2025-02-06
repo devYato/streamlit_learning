5588df72f6c278ac3aaadee46bef69266f40e0b0
Yato <renannjuina1907@outlook.com>
2025-02-05 20:52:50 -0500

    adding file download option for user

--- pages/2_examples.py
+++ pages/2_examples.py
@@ -1,11 +1,62 @@
 import os
 import streamlit as st
 import pandas as pd
-import numpy as np 
-import plotly.express as px
 from PIL import Image
+import logging, base64, time
 ##
 
+timestr = time.strftime("%Y%m%d-%H%M%S")
+## download file with function method
+def text_download():
+    b64 = base64.b64encode(texto.encode()).decode()
+    new_filename = f"new_text_file_{timestr}.txt"
+    href = f'<a href="data:file/txt;base64,{b64}" download="{new_filename}">Download file</a>'
+
+    st.markdown(href, unsafe_allow_html=True)
+    
+def csv_download(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    new_filename = f"new_csv_file_{timestr}.csv"
+    href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Download file</a>'
+
+    st.markdown(href, unsafe_allow_html=True)
+
+
+## download with Classe method
+class FileDownloader(object):
+    def __init__(self, file, filename, file_type):
+        self.file = file
+        self.file_name = filename
+        self.file_type = file_type
+        
+    def encode(self):
+        if isinstance(self.file, pd.DataFrame):
+            return self.file.to_csv(index=False).encode()
+        elif isinstance(self.file, str):
+            return self.file.encode()
+        else:
+            raise ValueError("Unsupported file type")
+
+    def download(self):
+        b64 = base64.b64encode(self.encode()).decode()
+        new_filename = f"{self.file_name}{timestr}.{self.file_type}"
+        href = f'<a href="data:file/{self.file_type};base64,{b64}" download="{new_filename}">Download file</a>'
+        st.markdown(href, unsafe_allow_html=True)
+    
+##logging
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+logs_dir = os.path.join(BASE_DIR, 'logs')
+os.makedirs(logs_dir, exist_ok=True)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+    handlers=[
+        logging.FileHandler(os.path.join(logs_dir, 'app.log')),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
 ## uploading a image
 menu = ['Home', 'Dataset', 'Docs', 'About']
 choice = st.sidebar.selectbox('Menu', menu)
@@ -20,6 +71,7 @@ def save_uploaded_file(uploaded_file):
         return st.success(f"Saved File:{uploaded_file.name} to C:/Windows/Temp")
 
 if choice == 'Home':
+    logging.info('Home')
     st.subheader('Home')
     img_file = st.file_uploader("Upload a image", type=['jpg', 'png', 'jpeg'], accept_multiple_files=True) # when accept multiple files, the return is a list
     if img_file is not None:
@@ -38,6 +90,7 @@ if choice == 'Home':
             st.write(i.name)
         
 elif choice == 'Dataset':
+    logging.info('Dataset')
     st.subheader('Dataset')
     data_file = st.file_uploader('Upload a CSV file', type=['CSV'])
     if data_file is not None:
@@ -49,6 +102,7 @@ elif choice == 'Dataset':
             st.dataframe(df)
             
 elif choice == 'Docs':
+    logging.info('Docs')
     st.subheader('Docs')
     st.write('Here is the documentation of your archive')
     doc = st.file_uploader('Upload a PDF, TXT or DOCX file', type=['PDF', 'TXT', 'DOCX'])
@@ -57,7 +111,7 @@ elif choice == 'Docs':
         ## i can work with the type of file, and show the contents depending on type
         details = {"type": doc.type, "name": doc.name, "size": doc.size}
         if st.button('Show details', key='details'):
-            st.write(details)
+            st.write(details) 
         if doc.type == 'text/plain':
             st.write('text file')
             #st.write(doc.read()) ## work but in bytes 
@@ -67,3 +121,42 @@ elif choice == 'Docs':
             st.write('pdf file')
             print(type(doc))
             print(doc)
+
+else:
+    tab1, tab2, tab3 = st.tabs(['download text file',
+                                'download csv file',
+                                'download xx file'])
+    with tab1:
+        st.image('https://www.streamlit.io/images/brand/streamlit-mark-color.png', width=100)
+        logging.info('About')
+        st.subheader('About')
+        st.write('This is a Streamlit app for uploading files')
+        st.write('Built with Streamlit and Python')
+        st.write('By: Renann santos')
+        st.write('Date: 2025-02-05')
+    
+        ## adding a file download option
+        # def update_file(text: str):
+        #     with open('C:/Windows/Temp/file.txt', 'w') as f:
+        #         f.write(str(text))
+        texto = st.text_area('Enter some text', placeholder='Type something')
+        st.markdown('#### Download file')
+        if st.button('Download file'):
+            '''
+            update_file(texto)
+            texto = texto.encode('utf-8')
+            b64 = base64.b64encode(texto).decode()
+            href = f'<a href="data:file/txt;base64,{b64}" download="file.txt">Download file</a>'
+            st.markdown(href, unsafe_allow_html=True)
+            '''
+            #text_download() - function method
+            download_text = FileDownloader(texto, 'new_text_file', 'txt').download()
+    with tab2:
+        st.title('download a csv file')
+        df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
+        if st.checkbox('Show dataframe'):
+            st.dataframe(df)
+        if st.button('Download CSV file'):
+            #download_csv = FileDownloader(df, 'new_csv_file', 'csv')
+            #download_csv.download()
+            download_csv = FileDownloader(df, 'new_csv_file', 'csv').download()
